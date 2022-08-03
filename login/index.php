85fb87bcba36872b958db38268c2d455461e6d75
Ichwanul Muslim <82445431+WeakBee@users.noreply.github.com>
2022-08-03 13:29:36 +0700

    benerin kodingan agung

--- login/index.php
+++ login/index.php
@@ -7,56 +7,46 @@
     <!-- BOOTSTRAP CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     <!-- BOOTSTRAP CSS -->
-	<link rel="stylesheet" href="../../style.css">
+	<link rel="stylesheet" href="style.css">
 </head>
 <body>
-<div class="cover" style="background-image:url(bg-login.png);">
+<div class="cover" style="background-image:url(../assets/Login/bg-login.png);">
 
 <!--page login-->
 <div class="page-login">
 
 <!--box-->
 <div class="box box-login">
+	<!--box body-->
+	<div class="box-body">
 	
-	
-		
-			
-			<!--box body-->
-			<div class="box-body">
-			
-			
-			<!--form login-->
-			<form action="" method="POST">
-			<img src = "icon-black.png";>
-			<h2>Sign In</h2>
-			<br>
-			<label>Login untuk masuk menggunakan akun yang telah terdaftar</label>
-			<br></br>
-			<div class="form-group">
+	<!--form login-->
+	<form action="" method="POST">
+		<img src="../assets/Login/icon-black.png">
+		<h2>Sign In</h2>
+		<br>
+		<label>Login untuk masuk menggunakan akun yang telah terdaftar</label>
+		<br></br>
+		<div class="form-group">
 			<input type="text" name="user" placeholder="Username" class="input-control">
-			</div>
-			
-			<div class="form-group">
-			
+		</div>
+
+		<div class="form-group">
 			<input type="password" name="pass" placeholder="Password" class="input-control">
-			</div>
-			<input type="checkbox" name="check" ><b> Remember Me </b><br></br>
-			<center>
-			<b><input type="submit" name="submit" value="Sign in" class="btn"></b>
-			</center>
-			<br>
-			<div>
-			<center>
-			<label><b>Belum Punya Akun ? </b> <a href="#">Daftar</a></label>
-			</center>
-			</div>
-			
-			
-			</form>
-			
-			
-			
-			
+		</div>
+
+		<input type="checkbox" name="check" ><b> Remember Me </b><br></br>
+		<center>
+		<b><input type="submit" name="submit" value="Sign in" class="btn"></b>
+		</center>
+		<br>
+		<div>
+		<center>
+		<label><b>Belum Punya Akun ? </b> <a href="#">Daftar</a></label>
+		</center>
+		</div>
+	</form>
+	
  <!-- BOOTSTRAP JS -->
  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
  <!-- BOOTSTRAP JS -->
