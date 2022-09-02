88b777aa4655d60f74054a8585a9878e6a9a8fca
Ichwanul Muslim <82445431+WeakBee@users.noreply.github.com>
2022-09-02 07:57:26 +0700

    bg dark navbar

--- navbar.js
+++ navbar.js
@@ -1,5 +1,6 @@
 const nav_bar = document.querySelector(".nav-bar");
 const logo = document.querySelector(".logo");
+const navbarbgdark = document.querySelector(".navbar-bg-dark");
 
 const responsive = window.matchMedia('(max-width: 992px)');
 
@@ -7,8 +8,10 @@ if (responsive.matches) {
     logo.onclick = function() {  
         if (nav_bar.hasAttribute("open")) {
             nav_bar.removeAttribute("open");
+            navbarbgdark.removeAttribute("open");
         } else {
             nav_bar.setAttribute("open", "");
+            navbarbgdark.setAttribute("open", "");
         }
     };  
 }
